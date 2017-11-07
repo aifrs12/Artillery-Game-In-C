02e260ef45c6358e5baad0bb3656bc4e825d8c59
Afonso Santos <aifrsantos12@gmail.com>
2017-11-07 19:26:50 +0000

    Create colors.h

---
+++ colors.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   colors.h
+ * Author: aifrsantos12
+ */
+
+#ifndef COLORES
+#define	COLORES
+
+#include <stdbool.h>
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define CORNORNAL  "\x1B[0m"
+#define CORVERMELHO  "\x1B[31m"
+#define CORVERDE  "\x1B[32m"
+#define CORAZUL "\x1b[34m"
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* COLORES */
